7bdbf0e5c13f6186563428c0481cbeafe82cda88
Marco U. Colón <marcocolon@live.com>
2020-01-23 17:03:51 -0800

    ball trajectory code

---
+++ MathCode/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+B
+Ball_Trajectory.py,c/9/c917db3c6f05d58eafd77ac723ceb90549af4c6b
---
+++ SourceCodeV1/src/main/java/frc/robot/Subsystems/BallTrajectory.java
@@ -0,0 +1,9 @@
+public class BallTrajectory {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
