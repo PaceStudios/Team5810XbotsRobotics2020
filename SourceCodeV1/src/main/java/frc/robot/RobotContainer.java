946eccfc2cc0fe8c9642736ded03bb1d90a6968f
PaceStudios2012@gamil.com <pacestudios2012@gmail.com>
2020-02-06 19:42:57 -0800

    Installed Wpilibj2

---
+++ SourceCodeV1/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,20 @@
+package frc.robot;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj2.*;
+
+
+
+
+public class RobotContainer{
+    public RobotContainer(){
+        init();
+    }
+    public void init(){
+        configureCommands();
+    }
+    public void configureCommands(){
+
+    }
+    
+
+}
