255485b01c04ccee86cf50be13c4fac1abe60e0f
ravila243 <55266416+ravila243@users.noreply.github.com>
2020-07-20 19:11:38 -0700

    Created hw1
    Homework 1 should consist of Strings, Floats and Ints, and Lists

---
+++ HW1/hw1.py
@@ -0,0 +1,39 @@
+from testcases import test_cases_q1, test_cases_q2
+
+''' Strings Practice 
+Fill in the '' to complete the code
+'''
+
+
+# QUESTION 1
+def hello_world():
+    """Below save a variable (message) as the string 'hello world'
+       and return that variable
+    >>> hello_world()
+    hello world
+    """
+    #START CODE HERE
+    message = ''
+    #END OF CODE
+    return message
+
+test_cases_q1(hello_world()) 
+print('')
+print('')
+
+
+
+# QUESTION 2
+def add_strings():
+  """Create a string variable ('balloon') return the sum of the string to itself
+  >>> add_strings()
+  balloonballoon
+  """
+  #START CODE HERE
+  message = ''
+  return ''
+  #END OF CODE
+
+test_cases_q2(add_strings())
+print('')
+print('')
